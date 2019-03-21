364a62f970c2dacb5d9075c7d41fc47cb2b256eb
Andrew <andrewcshike@gmail.com>
2019-03-21 12:26:52 -0500

    .ics upload and file read

--- map-events/map-events.php
+++ map-events/map-events.php
@@ -34,10 +34,115 @@ function extra_post_info_menu(){
 if( !function_exists("map_events_admin_page") )
 {
 function map_events_admin_page(){
+    test_handle_post();
 ?>
-  <h1>Hello. This is a page. Our functions will go here. :-0</h1>
+    <h1>CCA Facebook Events Importer</h1>
+    <h2>Upload a .ics File</h2>
+    <!-- Form to handle the upload - The enctype value here is very important -->
+    <form  method="post" enctype="multipart/form-data">
+            <input type='file' id='test_upload_ics' name='test_upload_ics'></input>
+            <?php submit_button('Upload') ?>
+    </form>
 <?php
 }
+}
+
+function test_handle_post(){
+        // First check if the file appears on the _FILES array
+        if(isset($_FILES['test_upload_ics'])){
+                $ics = $_FILES['test_upload_ics'];
+ 
+                // Use the wordpress function to upload
+                // test_upload_ics corresponds to the position in the $_FILES array
+                // 0 means the content is not associated with any other posts
+                $uploaded=media_handle_upload('test_upload_ics', 0);
+                // Error checking using WP functions
+                if(is_wp_error($uploaded)){
+                        echo "Error uploading file: " . $uploaded->get_error_message();
+                }else{
+                        echo "File upload successful!";
+                        convert($uploaded);
+                }
+        }
+}
+
+$events = array();
+$before = "/(?<=";
+$after = ")(?s)(.+?)(?=BEGIN:VCALENDAR|PRODID:|X-WR-CALNAME:|X-PUBLISHED-TTL:|X-ORIGINAL-URL:|VERSION:|CALSCALE:|METHOD:|BEGIN:VEVENT|DTSTAMP:|LAST-MODIFIED:|CREATED:|SEQUENCE:|ORGANIZER;|DTSTART:|DTEND:|UID:|SUMMARY:|LOCATION:|URL:|DESCRIPTION:|CLASS:|STATUS:|PARTSTAT:|END:VEVENT|END:VCALENDAR)/";
+$i = 0;
+
+function convert($id){
+    global $i, $events, $before, $after;
+    
+    $contents = file_get_contents(wp_get_attachment_url($id));
+    
+    $ics_events = explode("BEGIN:VEVENT", $contents);
+    array_shift($ics_events);
+
+    for ($i = 0; $i < count($ics_events); $i++) {
+        //global $events, $before, $after;
+
+        $events[$i] = array();
+
+        //$events[$i]["TEST"] = "fooey " . $i;
+
+        preg_replace_callback_array(
+            [
+                $before . "CREATED:" . $after => function($match) {
+                    global $i, $events;
+
+                    $events[$i]["CREATED"] = strtotime($match[0]);
+                },
+                $before . "ORGANIZER;" . $after => function($match) {
+                    global $i, $events;
+
+                    $events[$i]["ORGANIZER"] = preg_replace("/:MAILTO:.*|CN=/", "", $match[0]);
+                },
+                $before . "DTSTART:" . $after => function($match) {
+                    global $i, $events;
+
+                    $events[$i]["DTSTART"] = date("m/d/y g:i", strtotime($match[0]));
+                },
+                $before . "DTEND:" . $after => function($match) {
+                    global $i, $events;
+
+                    $events[$i]["DTEND"] = date("m/d/y g:i", strtotime($match[0]));
+                },
+                $before . "SUMMARY:" . $after => function($match) {
+                    global $i, $events;
+
+                    $events[$i]["SUMMARY"] = $match[0];
+                },
+                $before . "LOCATION:" . $after => function($match) {
+                    global $i, $events;
+
+                    $events[$i]["LOCATION"] = $match[0];
+                },
+                $before . "URL:" . $after => function($match) {
+                    global $i, $events;
+
+                    $events[$i]["URL"] = $match[0];
+                },
+                $before . "DESCRIPTION:" . $after => function($match) {
+                    global $i, $events;
+
+                    $events[$i]["DESCRIPTION"] = $match[0];
+                }
+            ],
+            $ics_events[$i]
+        );
+
+    //    array_push($events, $event);
+    }
+    //echo "<p>" . count($events) ."</p>";
+    for ($i = 0; $i < count($events); $i++) {
+        $stuff = $events[$i];
+        foreach ($stuff as $thing => $thing_value) {
+            echo "<p>$thing: $thing_value</p>";
+        }
+        //echo "<p>" . $events[$i] ."</p>";
+        echo "<br>";
+    }
 }
 ?>
  
